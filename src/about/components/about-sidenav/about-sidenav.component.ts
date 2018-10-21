8c64b056b8e8a9ac986e58f2e3f0474742af09b6
Nazmul Alam <nazmuldipu@gmail.com>
2018-10-21 20:21:43 +0600

    statice part partially done

---
+++ src/about/components/about-sidenav/about-sidenav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'about-sidenav',
+  templateUrl: './about-sidenav.component.html',
+  styleUrls: ['./about-sidenav.component.scss']
+})
+export class AboutSidenavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
